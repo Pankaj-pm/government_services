3877bc9ab454b8cdb08390b354dc8ca1827c08d8
Flutter <abc@example.com>
2023-09-15 19:21:26 +0530

    in app webview

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:flutter/material.dart';
 import 'package:government_services/connection_provider.dart';
+import 'package:government_services/gov_services.dart';
 import 'package:provider/provider.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(const MyApp());
 }
 
@@ -16,6 +18,7 @@ class MyApp extends StatelessWidget {
       create: (context) => ConnectionProvider(),
       builder: (context, child) => MaterialApp(
         title: 'Flutter Demo',
+        debugShowCheckedModeBanner: false,
         theme: ThemeData(
           primarySwatch: Colors.blue,
         ),
@@ -38,10 +41,11 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   void initState() {
     super.initState();
-   Connectivity().onConnectivityChanged.listen((event) {
-     bool isConnection= event!=ConnectivityResult.none;
-     Provider.of<ConnectionProvider>(context,listen: false).connectionChange(isConnection);
-   });
+    Connectivity().onConnectivityChanged.listen((event) {
+      bool isConnection = event != ConnectivityResult.none;
+      Provider.of<ConnectionProvider>(context, listen: false)
+          .connectionChange(isConnection);
+    });
   }
 
   @override
@@ -60,6 +64,12 @@ class _MyHomePageState extends State<MyHomePage> {
                   child: Container(
                     margin: EdgeInsets.all(10),
                     color: Colors.red,
+                    child: Center(
+                      child: Text(
+                        "umang",
+                        style: TextStyle(color: Colors.white, fontSize: 28),
+                      ),
+                    ),
                   ),
                 ),
               ),
@@ -69,6 +79,12 @@ class _MyHomePageState extends State<MyHomePage> {
                   child: Container(
                     margin: EdgeInsets.all(10),
                     color: Colors.red,
+                    child: Center(
+                      child: Text(
+                        "india.gov.in",
+                        style: TextStyle(color: Colors.white, fontSize: 28),
+                      ),
+                    ),
                   ),
                 ),
               ),
@@ -79,23 +95,69 @@ class _MyHomePageState extends State<MyHomePage> {
               Expanded(
                 child: AspectRatio(
                   aspectRatio: 1,
-                  child: Container(
-                    margin: EdgeInsets.all(10),
-                    color: Colors.red,
+                  child: InkWell(
+                    onTap: () {
+                      Navigator.push(context, MaterialPageRoute(
+                        builder: (context) {
+                          return CovServices(
+                            url: "https://www.rmc.gov.in/",
+                          );
+                        },
+                      ));
+                    },
+                    child: Container(
+                      margin: EdgeInsets.all(10),
+                      color: Colors.red,
+                      child: Center(
+                          child: Text(
+                        "rmc",
+                        style: TextStyle(color: Colors.white, fontSize: 28),
+                      )),
+                    ),
                   ),
                 ),
               ),
               Expanded(
                 child: AspectRatio(
                   aspectRatio: 1,
-                  child: Container(
-                    margin: EdgeInsets.all(10),
-                    color: Colors.red,
+                  child: InkWell(
+                    onTap: () {
+                      Navigator.push(context, MaterialPageRoute(
+                        builder: (context) {
+                          return CovServices(
+                            url:
+                                "https://dot.gov.in/national-government-services-portal",
+                          );
+                        },
+                      ));
+                    },
+                    child: Container(
+                      margin: EdgeInsets.all(10),
+                      color: Colors.red,
+                      child: Center(
+                        child: Text(
+                          "national-government",
+                          style: TextStyle(color: Colors.white, fontSize: 28),
+                        ),
+                      ),
+                    ),
                   ),
                 ),
               ),
             ],
-          )
+          ),
+          ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => CovServices(
+                      url: "https://www.google.com/",
+                    ),
+                  ),
+                );
+              },
+              child: Text("Google"))
         ],
       ),
       // bottomNavigationBar: StreamBuilder<ConnectivityResult>(
@@ -115,18 +177,18 @@ class _MyHomePageState extends State<MyHomePage> {
       // ),
       bottomNavigationBar: Consumer<ConnectionProvider>(
         builder: (context, value, child) {
-          if(value.isConnection){
+          if (value.isConnection) {
             return SizedBox();
           }
           return Container(
-          color: value.isConnection ? Colors.green : Colors.red,
-          height: 30,
-          alignment: Alignment.center,
-          child: Text(
-            value.isConnection ? "Connected" : "No connection ",
-            style: TextStyle(color: Colors.white),
-          ),
-        );
+            color: value.isConnection ? Colors.green : Colors.red,
+            height: 30,
+            alignment: Alignment.center,
+            child: Text(
+              value.isConnection ? "Connected" : "No connection ",
+              style: TextStyle(color: Colors.white),
+            ),
+          );
         },
       ),
       floatingActionButton: FloatingActionButton(
